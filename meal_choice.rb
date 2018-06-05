f09be7ec281fd429c1679d032191bc3ea93cc2c9
caitlinmch <caitlinmch@me.com>
2018-06-05 18:01:02 +0000

    automatically backed up by learn

---
+++ meal_choice.rb
@@ -0,0 +1,12 @@
+def breakfast(food = "eggs")
+  puts "Morning is the right time for #{food}!" 
+end
+breakfast("pancakes")
+def lunch(lunch = "salad")
+  puts "Lunch is the best time for #{lunch}!"
+end
+lunch("sandwiches")
+def dinner(dinner = "cassarole")
+  puts "Dinner is the best time for #{dinner}!"
+end
+dinner("pasta")
